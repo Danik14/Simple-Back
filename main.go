360a0aa53914cc14066045fb70995338e6116805
Danik <chapagana@gmail.com>
2022-10-07 10:47:10 +0600

    Add simple validation and specified url params for snippetView

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	//making path exact
+	//if leave it without it
+	//any url string will be sufficient to activate this handler
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -16,23 +21,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+
+	//validating id string, because it is entered by user
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Snipped with id value of %v", id)
 }
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
+	//making this handler to only allow post requests
 	if r.Method != "POST" {
-		w.Header().Set("Allowsf", "POSsT")
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+		w.Header().Set("Allowsf", http.MethodPost)
+		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	w.Write([]byte("Create a new snippet..."))
 }
 func main() {
-	// Register the two new handler functions and corresponding URL patterns with // the servemux, in exactly the same way that we did before.
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
