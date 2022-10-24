f8aaf03418ad60957c0a8d2985921645bd637736
Danik <chapagana@gmail.com>
2022-10-24 15:32:26 +0600

    Add user authentication

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -15,17 +15,26 @@ var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9
 
 // Define a new Validator type which contains a map of validation errors for our // form fields.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
-// AddFieldError() adds an error message to the FieldErrors map (so long as no // entry already exists for the given key).
+// Create an AddNonFieldError() helper for adding error messages to the new
+// NonFieldErrors slice.
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+// AddFieldError() adds an error message to the FieldErrors map (so long as no
+// entry already exists for the given key).
 func (v *Validator) AddFieldError(key, message string) {
-	// Note: We need to initialize the map first, if it isn't already // initialized.
+	// Note: We need to initialize the map first, if it isn't already
+	// initialized.
 	if v.FieldErrors == nil {
 		v.FieldErrors = make(map[string]string)
 	}
